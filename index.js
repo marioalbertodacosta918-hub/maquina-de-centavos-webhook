b2d8b705ce0a1ac6b8c69a2b9f205f8120013072
marioalbertodacosta918-hub <marioalbertodacosta918@gmail.com>
2026-08-24 17:34:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+  console.log("Webhook recebido:", req.body);
+
+  res.status(200).send("OK");
+});
+
+app.get("/", (req, res) => {
+  res.send("Máquina de Centavos - Webhook funcionando!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando na porta ${PORT}`);
+});
