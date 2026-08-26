dab6d34ebb6a3609a2b7563c519af79cdcf29faa
marioalbertodacosta918-hub <marioalbertodacosta918@gmail.com>
2026-08-26 15:38:51 -0300

    Update create-preference.js

--- api/create-preference.js
+++ api/create-preference.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
+  if (req.method !== "GET" && req.method !== "POST") {
     return res.status(405).json({
       error: "Método não permitido"
     });
